36cd8a91129c2f58a783249756fd385fefd62162
Patrick Sheridan <sheridp2@gmail.com>
2021-07-15 10:47:35 -0700

    rerouting banner added

--- public/js/init.js
+++ public/js/init.js
@@ -1,10 +1,8 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
+(function ($) {
+  $(function () {
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-console.log('Nosey one, aren\'t you?');
+console.log("Nosey one, aren't you?");
